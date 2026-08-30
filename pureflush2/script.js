8a889cdf51b224c4519da309bcefb17dc432e130
valkenlee <39821371+valkenlee@users.noreply.github.com>
2026-08-30 15:21:38 +0900

    Update script.js

--- pureflush2/script.js
+++ pureflush2/script.js
@@ -79,11 +79,12 @@ window.addEventListener('DOMContentLoaded', async () => {
 // ==========================================
 const GAS_CONFIG = {
     // 1. 배포된 Google Apps Script Web App URL
-    apiUrl: "https://script.google.com/macros/s/YOUR_GAS_DEPLOYMENT_ID/exec",
+    apiUrl: "https://script.google.com/macros/s/AKfycbwsEG416KzPEf7ReF_7G5eGzce3x7OvYwePke91Vd4POR2svkdgvzEoCQFtg3HWzVJ1EQ/exec
+",
     // 2. Apps Script의 SECRET_KEY와 반드시 동일하게 설정
     secretKey: "Mahjong_Quiz_Secret_Key_2026",
     // 3. 구글 시트 웹 게시 CSV URL (읽기용)
-    csvUrl: "https://docs.google.com/spreadsheets/d/e/YOUR_SHEET_ID/pub?gid=0&single=true&output=csv"
+    csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTnJU4yDCDyeZZCmpkbogFP62WF_AcmsitYv6YBufHxY2qafrzmqXjvOHUrAsGp0sjeK-FBAptasrpq/pub?gid=1559316332&single=true&output=csv"
 };
 
 /* -------------------------------------------------------------
