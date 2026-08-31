24680175ce673c82be92a837882b0d3f6d86ab96
valkenlee <39821371+valkenlee@users.noreply.github.com>
2026-08-31 21:26:36 +0900

    Update script.js

--- pureflush3/script.js
+++ pureflush3/script.js
@@ -1,7 +1,7 @@
 /* =============================================================
    📌 (Last Updated: 2026-08-31)
    ============================================================= */
-const APP_VERSION = "0.4.2";
+const APP_VERSION = "0.4.3";
 console.log(`[App Initialized] Version: ${APP_VERSION}`);
 
 let zipInstance = null;
